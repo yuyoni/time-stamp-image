51d792697c5d6e1d943f6512028ee589d070e0f4
yuyoni <doli0116@naver.com>
2024-01-15 03:35:14 +0900

    🚑 BugFix : font slow load issue 해결

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -6,17 +6,23 @@ const GlobalStyle = createGlobalStyle`
     src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/GmarketSansMedium.woff') format('woff');
     font-weight: normal;
     font-style: normal;
-}
+    font-display: block;
+  }
 
   @font-face{
-  font-family:'DNFBitBitv2';
-  font-style:normal;font-weight:400;src:url('//cdn.df.nexon.com/img/common/font/DNFBitBitv2.otf')format('opentype')}
+    font-family:'DNFBitBitv2';
+    src: url('https://cdn.df.nexon.com/img/common/font/DNFBitBitv2.otf') format('opentype');
+    font-style: normal;
+    font-weight:400;
+    font-display: block;
+  }
 
   @font-face {
       font-family: 'DOSGothic';
       src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_eight@1.0/DOSGothic.woff') format('woff');
       font-weight: normal;
       font-style: normal;
+      font-display: block;
   }
 
   body {
