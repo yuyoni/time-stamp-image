5733627bbcd23b92b759bed5f449414c176567a3
yuyoni <doli0116@naver.com>
2024-01-22 03:22:09 +0900

    Feat : 달력 옵션 추가
    - 직관적인 UI를 위한 달력 아이콘 추가
    - 사용자 편의를 위한 month dropdown 옵션, 오늘날짜로 가기 버튼 추가
    - 좀 더 세분화된 커스텀을 위해 시간 선택 옵션 추가

--- src/components/buttons/CustomDateButton.jsx
+++ src/components/buttons/CustomDateButton.jsx
@@ -1,4 +1,4 @@
-import ReactDatePicker from "react-datepicker";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import styled from "styled-components";
 
@@ -10,15 +10,21 @@ export default function CustomDateButton({
   return (
     uploadedInfo && (
       <Wrapper>
-        <ReactDatePicker
+        <DatePicker
+          showIcon // 달력 아이콘 보임
+          showMonthDropdown // month dropdown으로 볼 수 있음
+          useShortMonthInDropdown // month 줄임말로
           className="datePicker"
-          showTimeSelect
+          showTimeSelect // 시간 선택 가능
           dateFormat="yyyy.MM.dd" // 날짜 형태
           shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
           minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
           maxDate={new Date()} // maxDate 이후 날짜 선택 불가
           selected={selectedDate ? selectedDate : new Date()}
-          onChange={(date) => setSelectedDate(date)}
+          onChange={(date) => {
+            setSelectedDate(date);
+          }}
+          todayButton="SET DATE TODAY"
         />
       </Wrapper>
     )
@@ -27,4 +33,5 @@ export default function CustomDateButton({
 
 const Wrapper = styled.div`
   grid-area: date-picker;
+  z-index: 3;
 `;
