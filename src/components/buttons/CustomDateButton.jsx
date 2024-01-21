68de86109d8527e4616e453ebaacf859eb6e26da
yuyoni <doli0116@naver.com>
2024-01-22 02:56:19 +0900

    Feat : Time 옵션 추가

--- src/components/buttons/CustomDateButton.jsx
+++ src/components/buttons/CustomDateButton.jsx
@@ -12,6 +12,7 @@ export default function CustomDateButton({
       <Wrapper>
         <ReactDatePicker
           className="datePicker"
+          showTimeSelect
           dateFormat="yyyy.MM.dd" // 날짜 형태
           shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
           minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
